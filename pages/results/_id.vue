9bc4363c0f71d14c85ca90495c6ea6f9f8535c86
Dorian Valadez <dorian_v_d@hotmail.com>
2018-09-04 02:16:00 -0500

    Parte 3:Importar datos Album y Tienda

--- pages/results/_id.vue
+++ pages/results/_id.vue
@@ -1,5 +1,19 @@
 <template>
     <div>
     <h1>Hola desde la ruta de resultados {{$route.params.id}}</h1>
+    <h2>Datos del Album</h2>
+    {{albumData}}
     </div>
 </template>
+<script>
+import axios from 'axios';
+export default {
+    asyncData(params){
+    return axios.get(`https://itunes.apple.com/search?term=${params.id}&entity=album`)
+    .then((response) => {
+      return {albumData: response.data.results}
+    });
+    },
+        middleware: 'search'
+}
+</script>
